11648f598a29219cc513f8d5776b30f8d3976a75
Nightmonger <68603342+Nightmonger@users.noreply.github.com>
2023-02-02 12:25:08 +0200

    done.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw-gtor';
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = '';
+
+  onButtonClick(){
+  const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()_+';
+    let validChars ='';
+
+    if (this.includeLetters){
+      validChars += letters;
+    }
+    if(this.includeNumbers){
+      validChars+=numbers;
+    }
+    if(this.includeSymbols){
+      validChars+=symbols;
+    }
+
+    let generatedPwd = '';
+    for (let i =0 ; i<this.length; i++){
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPwd += validChars[index];
+      this.password = generatedPwd;
+
+    }
+
+  }
+
+  onChangeLetters(){
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeNumbers(){
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeSymbols(){
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onChangeLength(target : EventTarget) {
+    const value  = (<HTMLInputElement>target).value;
+    const parsedValue = parseInt(value);
+
+    if(!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
+
+
+
+
+
 }
